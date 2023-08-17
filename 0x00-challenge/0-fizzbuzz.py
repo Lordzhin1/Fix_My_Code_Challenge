69285e64b50c58bfae0ff0b82df4949a4f3f65cd
Your Name <your.email@example.com>
2023-08-17 14:18:28 -0700

    alx_Fix_My_Code_Challenge

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -10,30 +10,24 @@ def fizzbuzz(n):
 
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
-    if n < 1:
-        return
-
-    tmp_result = []
-    for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
-        else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+    - For numbers which are multiples of both three and five print "FizzBuz)"."""
+    import sys
 
 
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
+if len(sys.argv) != 2:
+    sys.exit(1)
+if not sys.argv[1].isdigit():
+    sys.exit(1)
+if int(sys.argv[1]) < 1:
+    sys.exit(1)
 
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+for i in range(1, int(sys.argv[1]) + 1):
+    output = ""
+    if i % 3 == 0:
+        output += "Fizz"
+    if i % 5 == 0:
+        output += "Buzz"
+    if not output:
+        output = i
+    print(output, end=" ")
+print()
